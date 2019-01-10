1e07844e1f8c5bcfcaad6510e4309e10f6a82e6a
Matt Miller <matt@cycronix.com>
2019-01-10 13:56:40 -0500

    collision stats

--- Assets/Scripts/Game/KillSwitch.cs
+++ Assets/Scripts/Game/KillSwitch.cs
@@ -16,12 +16,16 @@ limitations under the License.
 
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class KillSwitch : MonoBehaviour
 {
     private CTunity ctunity;
+    private CTstats ctstats;
+    public int maxHits = 0;        // max hits before vulnerable to be killed
+    public int hitLevel = 10;
 
     // Use this for initialization
     void Start()
@@ -31,16 +35,21 @@ public class KillSwitch : MonoBehaviour
     }
 
     //----------------------------------------------------------------------------------------------------------------
-
-    void OnCollisionEnter(Collision other)
+    private void setStats()
     {
-        //        Debug.Log(name + ": collision with: " + other.gameObject.name);
-        // if other is bullet or this is bullet, bang!
-//        if ((other.gameObject.tag == "Bullet" /* || gameObject.tag == \"Bullet\" */) && ctunity.activePlayer(gameObject))
-       if ((other.gameObject.tag == "Bullet") && ctunity.activePlayer(gameObject) && !ctunity.localPlayer(other.gameObject))
+        if (ctstats != null) return;  // got it
+
+        string stats = ctunity.Player + "/Stats";
+        GameObject sgo = GameObject.Find(stats);
+        if (sgo != null)
         {
-//                Debug.Log(name + ": Killed by: " + other.gameObject.name);
-                ctunity.clearObject(gameObject, false);
+            ctstats = sgo.GetComponent<CTstats>();
+        }
+
+        if (ctstats != null)
+        {
+            ctstats.hits++;
+ //           Debug.Log(name + ", ctstats.hits: "+ ctstats.hits);
         }
     }
 
@@ -48,13 +57,45 @@ public class KillSwitch : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-//        Debug.Log(name + ": trigger with: " + other.gameObject.name);
-        // if other is bullet or this is bullet, bang!
-        //        if (other.gameObject.tag == "Bullet" /* || gameObject.tag == "Bullet" */)
-        if ((other.gameObject.tag == "Bullet" ) && ctunity.activePlayer(gameObject) && !ctunity.localPlayer(other.gameObject))
+//        Debug.Log(name + ", Trigger with: " + other.name);
+        doCollision(other);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+ //       Debug.Log(name + ", Collision with: " + collision.collider.name);
+        doCollision(collision.collider);
+    }
+
+    //----------------------------------------------------------------------------------------------------------------
+    void doCollision(Collider other)
+    {
+        if(ctunity == null) ctunity = GameObject.Find("CTunity").GetComponent<CTunity>();
+        if (other.gameObject == null || ctunity == null)
         {
-//           Debug.Log(name + ": killed by: " + other.gameObject.name);
-            ctunity.clearObject(gameObject, false);
+            Debug.Log(name + ": OnTrigger null other object: "+other.name);
+            return;
+        }
+
+        // compare hit levels to see who wins
+        int otherHitLevel = 0;
+        KillSwitch kso = other.gameObject.GetComponent<KillSwitch>();
+        if (kso != null)
+        {
+            otherHitLevel = kso.hitLevel;
+
+            if ((hitLevel < otherHitLevel) && ctunity.activePlayer(gameObject) && !ctunity.localPlayer(other.gameObject))
+            // if ((other.gameObject.tag == "Bullet") && ctunity.activePlayer(gameObject) && !ctunity.localPlayer(other.gameObject))
+            {
+                setStats();
+     //           Debug.Log(name + ": HIT by: " + other.gameObject.name + ", hitLevel: "+ hitLevel+", ohl: "+otherHitLevel);
+
+                if (ctstats==null || ctstats.hits >= maxHits)
+                {
+                    // Debug.Log(name + ": killed!");
+                    ctunity.clearObject(gameObject, false);  // can't destroyImmediate inside collision callback
+                }
+            }
         }
     }
 }
